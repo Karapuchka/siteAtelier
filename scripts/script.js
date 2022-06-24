b15ae07e97af02bef4560e0e31f76948757d8587
Karapuchka <77234090+Karapuchka@users.noreply.github.com>
2022-06-24 15:56:29 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ const navBurgerBtn = document.querySelector('.burger-btn'),
         burgerBox = document.querySelector('.burger'),
         headerOffer = document.querySelector('.header-offer');
 
-navBurgerBtn.addEventListener('pointerdown', ()=>{
+navBurgerBtn.addEventListener('pointerdown', function(event){
         if(burgerBox.classList.contains('burger--emergence')){
                 
                 //Код для анимации меню-бургер
@@ -32,3 +32,58 @@ navBurgerBtn.addEventListener('pointerdown', ()=>{
         }
 });
 
+const headerTitleH1 = document.querySelector('.header-offer__bg h1'),
+        headerTitleH4 = document.querySelector('.header-offer__bg h4'),
+        headerTitleBtn = document.querySelector('.header-offer__bg .btn'),
+        mainTitleH2Array = document.querySelectorAll('.main-title h2'),
+        mainTitleH6Array = document.querySelectorAll('.main-title h6');
+        galaryItemArray = document.querySelectorAll('.galary__item');
+
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener('DOMContentLoaded', function(event){
+
+        const headerTitleTl = gsap.timeline({defaults: {duration: 1, opacity: 0, ease: 'back.out(1.7)'}});
+
+        headerTitleBtn.style.opacity = '1';//Без этой инструкции кнопка при загрузке сначало видна и только через секунду исчезает
+
+        headerTitleTl.from(headerTitleH1, {delay: .8})
+                .from(headerTitleH4, {y: 40})
+                .from(headerTitleBtn, {delay: .1});
+                
+        
+        for (const iterator of mainTitleH2Array) {
+                  gsap.from(iterator, { 
+                        scrollTrigger: {
+                                trigger: iterator,
+                                toggleActions: 'restart restart restart restart',
+                        },
+                        duration: 1, y: 50, opacity: 0, ease: 'back.out(1.7)',
+                });
+        };
+
+        for (const iterator of mainTitleH6Array) {
+                gsap.from(iterator, {
+                        scrollTrigger:{
+                                trigger: iterator,
+                                toggleActions: 'restart restart restart restart',
+                        },
+                        duration: 1, y: 50, opacity: 0, ease: 'back.out(1.7)', delay: .5,
+                })
+        }
+
+
+        
+        for (const iterator of galaryItemArray) {
+                gsap.from(iterator, {
+                        scrollTrigger: {
+                                trigger: iterator,
+                                toggleActions: 'restart restart restart restart',
+                                start: 'top 80%',
+                                end: 'bottom 50%',
+                                scrub: true,
+                        },
+                        duration: .5, opacity: 0, ease: 'flower1 out',
+                })
+        }
+});
